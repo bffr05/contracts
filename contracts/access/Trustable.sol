c2682d28d609d48866acd9ee8ce08aa98a515d7a
Benjamin <benjamin@macbook.home>
2023-01-23 10:07:17 +0100

    Update Trustable.sol

--- contracts/access/Trustable.sol
+++ contracts/access/Trustable.sol
@@ -68,7 +68,7 @@ contract Trustable is Ownable,Location, Referral, ITrustable, IRTrustable {
             return super.referral();
         if (locator().code.length == 0)
             return address(0);
-        out_ = location(kAccess);
+        out_ = get(kAccess);
         if (out_==address(this))
             out_=address(0);
     }
