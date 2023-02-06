83d05e927aef2c46555cdfdc6fb6b8cc9c9cc229
Benjamin <benjamin@macbook.home>
2023-02-06 20:38:22 +0100

    Update Locator.sol

--- contracts/utils/Locator.sol
+++ contracts/utils/Locator.sol
@@ -16,13 +16,14 @@ address constant LocatorAddress = 0x455b153B592d4411dCf5129643123639dcF3c806;
 interface ILocator {
     function location(bytes32 hash_) external view returns (address out_);
     function get(bytes32 hash_) external view returns (address out_);
-    function get(string memory arg_) external view returns (address out_);
+    //function get(string memory arg_) external view returns (address out_);
     function set(bytes32 hash_, address addr_) external;
-    //function set(string memory arg_, address addr_) external;
-    function hash(string memory arg_) external  pure returns (bytes32);
+    function set(string memory arg_, address addr_) external;
+    function hash(string memory arg_) external  view returns (bytes32);
+    function hash( address writer_, string memory arg_) external view returns (bytes32);
 }
 contract Locator is Ownable,ILocator {
-    bool immutable writeonce = false;
+    //bool immutable writeonce = false;
     mapping(bytes32 => address) internal _location;
 
     function location(bytes32 hash_) public view returns (address out_) {
@@ -35,20 +36,20 @@ contract Locator is Ownable,ILocator {
         if (out_==address(0))
             out_ = _location[hash_];
     }
-    function get(string memory arg_) public view returns (address out_) {
-        return get(keccak256(abi.encodePacked(arg_)));
-    }
-
     function set(bytes32 hash_, address addr_) public onlyOwner {
-        require(!(_location[hash_]!=address(0) && writeonce), "Location write once");
         _location[hash_] = addr_;
     }
-    /*function set(string memory arg_, address addr_) public onlyOwner() {
-        set(keccak256(abi.encodePacked(arg_)),addr_);
-    }*/
-     
-    function hash(string memory arg_) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(arg_));
+    function set(string memory arg_, address addr_) public {
+        _location[hash(arg_)] = addr_;
+    }
+    function hash( address writer_, string memory arg_) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(writer_,arg_));
+    }
+    function hash(string memory arg_) public view returns (bytes32) {
+        if (_msgSender()==ownerOf())
+            return keccak256(abi.encodePacked(arg_));
+        else
+            return hash(_msgSender(),arg_);
     }
 }
 interface ILocation {
